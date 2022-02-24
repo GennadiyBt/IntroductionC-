94526a62fbe1a406c36c79b04f8a4cf2c92488d7
Gennadiy <safandsec71@gmail.com>
2022-02-24 11:37:19 +0300

    Проверка на квадрат2

--- SeminarC#1/Program.cs
+++ SeminarC#1/Program.cs
@@ -2,7 +2,7 @@
 int value1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:");
 int value2 = Convert.ToInt32(Console.ReadLine());
-if (value1*value1 == value2)
+if (value2*value2 == value1)
 {
     Console.WriteLine("Да, является");
 }
