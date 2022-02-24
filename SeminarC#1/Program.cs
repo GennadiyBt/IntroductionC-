04e9ed2df3f7c0be3c03a33acde3159eec2c678b
Gennadiy <safandsec71@gmail.com>
2022-02-24 11:32:29 +0300

    Проверка на квадрат

---
+++ SeminarC#1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число:");
+int value1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int value2 = Convert.ToInt32(Console.ReadLine());
+if (value1*value1 == value2)
+{
+    Console.WriteLine("Да, является");
+}
+else
+{
+    Console.WriteLine("Нет, не является");
+}
